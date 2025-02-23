06f6bc0e84afe660debaa51c4d45d26bd768b99f
Thayanne Soares <133281353+MisterEu09@users.noreply.github.com>
2025-02-23 12:49:28 -0300

    update

---
+++ src/javascript/menu.js
@@ -0,0 +1,19 @@
+[
+  {
+    "image": "src/images/1.png",
+    "name": "Lorem Ipsum 1",
+    "description": "Descrição do prato 1.",
+    "rating": 5,
+    "reviews": 500,
+    "price": 20.00
+  },
+  {
+    "image": "src/images/2.png",
+    "name": "Lorem Ipsum 2",
+    "description": "Descrição do prato 2.",
+    "rating": 4,
+    "reviews": 320,
+    "price": 25.50
+  },
+  // ... outros pratos
+]
